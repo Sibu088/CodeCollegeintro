58f9ff8c621f1c72d388a6b1bd716e22089d8b99
Sibu088 <mavismadira6@gmail.com>
2025-01-23 20:51:05 +0200

    chapter 12-14

--- Dummies/Chapter 11/main.js
+++ Dummies/Chapter 11/main.js
@@ -1,17 +1,15 @@
-var parents =["Cadee","Ulrich"];
-var friends =["Lesedi","Ephodia"];
-
+var parents = ["Cadee", "Ulrich"];
+var friends = ["Lesedi", "Ephodia"];
 
 var family = parents.concat(friends).join(" ");
 
 var familia = document.getElementById("myFamily");
-let longArray =[1,2,3,4,5,6,7,8,9,10];
+let longArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(family);
 console.log(longArray.lastIndexOf(6));
-console.log(longArray.slice(3,0,"David"));
-ifamiliannerHTML ="<h1>this is my family:" + family + "</h1>";
+console.log(longArray.slice(1, 0, "David"));
+ifamiliannerHTML = "<h1>this is my family:" + family + "</h1>";
 
-console.log(longArray.slice(2,7));
+console.log(longArray.slice(2, 7));
 
 console.log(longArray.slice(7, 9));
-
